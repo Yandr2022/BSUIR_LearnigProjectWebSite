42f25b57dcea9ee064ff7d7c038751b3f59be369
Yandr2022 <sharkievich@gmail.com>
2024-08-13 09:23:38 +0300

    added news page

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,17 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+const details = document.querySelectorAll('details');
+
+details.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const openItem = document.querySelector('details[open]');
+      
+    if (openItem) openItem.open = false;
+    
+    if (openItem !== item) item.open = true 
+  })
+})
